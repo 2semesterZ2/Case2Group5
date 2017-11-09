c47225d5f63bdaed8024b528abe8fd8509aad8e6
UnkrautFee <kraftsa60869@th-nuernberg.de>
2017-11-09 12:05:15 +0100

    Bugfix
    Bugfix

--- Project/uploadAddOverlay.php
+++ Project/uploadAddOverlay.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <title>Uploading a Photo</title>
-    <link rel="stylesheet" type="text/css" href="common.css" />
+    <link rel="stylesheet" type="text/css" href="style.css" />
   </head>
   <body>
 
